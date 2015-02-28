f1df8992f3049047e88df8e0cf39bf5720a259c4
rporrini <riccardo.porrini@disco.unimib.it>
2015-02-28 11:45:28 +0100

    empowered the integration test

--- scripts/test-summarization-pipeline.sh
+++ scripts/test-summarization-pipeline.sh
@@ -5,16 +5,32 @@ function assert_no_errors_on()
 	file=$1
 	
 	highlight_color='\e[0;32m'
-	message='TEST PASSED'
-
+	message='OK'
+	
 	if [[ $(grep "Command exited with non-zero status 1" $file) ]]
 	then
 		highlight_color='\e[0;31m'
-		message="TEST FAILED: there were errors in log file ${file}"
+		message="KO. There were errors in log file ${file}"
 	fi
-	echo
-	echo -e "${highlight_color}${message}"
-	echo
+	echo -e "checking that execution was successful: ${highlight_color}${message}\e[0m"
+}
+
+function assert_results_are_compliant()
+{
+	expected=$1
+	actual=$2
+	
+	highlight_color='\e[0;32m'
+	message='OK'
+	
+	if [[ $(diff -qr $expected $actual) ]]
+	then
+		highlight_color='\e[0;31m'
+		message="KO"
+	fi
+	echo -e "checking that execution produced the correct result: ${highlight_color}${message}"
+	echo $(diff -qr $expected $actual)	
+	echo -e "\e[0m"
 }
 
 function as_absolute(){
@@ -30,12 +46,16 @@ relative_path=`dirname $0`
 current_directory=$(as_absolute $relative_path)
 root=$(as_absolute $current_directory/../)
 data=$root/benchmark/integration-test
-results=$root/benchmark/integration-test-results
+results=$root/benchmark/tmp
+expected_results=$root/benchmark/integration-test-results
 
 cd $current_directory
 ./run-summarization-pipeline.sh $data $results
-assert_no_errors_on ../summarization/log/log.txt
 
+echo
+assert_no_errors_on ../summarization/log/log.txt
+assert_results_are_compliant $expected_results $results
+echo
 
 
 

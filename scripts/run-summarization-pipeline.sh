a7dfea976658d675921090f621712239e61b5173
rporrini <riccardo.porrini@disco.unimib.it>
2015-02-28 10:48:47 +0100

    fixed a bug in the scripts for running the whole pipeline

--- scripts/run-summarization-pipeline.sh
+++ scripts/run-summarization-pipeline.sh
@@ -5,8 +5,12 @@ function as_absolute(){
 }
 
 set -e
+
+mkdir -p $1 $2
 relative_path=`dirname $0`
 current_directory=$(as_absolute $relative_path)
+data=$(as_absolute $1)
+results=$(as_absolute $2)
 
 echo "building the java summarization module"
 
@@ -16,10 +20,6 @@ cd "$current_directory"
 echo "done"
 
 echo "running the summarization pipeline"
-
-data=$(as_absolute $1)
-results=$(as_absolute $2)
-
 echo "with data from $data"
 echo "saving results in $results"
 

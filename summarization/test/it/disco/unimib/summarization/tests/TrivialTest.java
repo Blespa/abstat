7b3e2c7ca056875dc459830fe43789633cb59e79
rporrini <riccardo.porrini@disco.unimib.it>
2015-03-05 11:23:30 +0100

    added unit tests to the summarization project

---
+++ summarization/test/it/disco/unimib/summarization/tests/TrivialTest.java
@@ -0,0 +1,14 @@
+package it.disco.unimib.summarization.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrivialTest {
+
+	@Test
+	public void shouldAlwaysPass() throws Exception {
+		
+		assertTrue(true);
+	}
+}

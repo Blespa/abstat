e37c1f4cf190c2e167777a8a853b8c8031966b45
rporrini <riccardo.porrini@disco.unimib.it>
2015-03-10 00:49:40 +0100

    refactoring of the sparql engine

--- web/assets/js/controllers.js
+++ web/assets/js/controllers.js
@@ -1,37 +1,62 @@
 var summary = angular.module('schemasummaries', []);
 
 summary.controller('Summarization', function ($scope, $http, $location) {
-	$scope.selected_graph='Select a dataset'
-	
-	$http.get('http://' + $location.host() + ':8890/sparql', {
-        method: 'GET',
-        params: {
-            query: "select distinct ?uri where {GRAPH ?uri {?s ?p ?o} . FILTER regex(?uri, 'schemasummaries')}",
-            format: 'json'
-        }
-    }).success(function(res){
-        $scope.graphs=res.results.bindings;
-    });
 	
 	$scope.loadPatterns= function(){
-		$http.get('http://' + $location.host() + ':8890/sparql', {
+		
+		new Sparql($http, $location)
+			.query('prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ' +
+			        'prefix ss:   <http://schemasummaries.org/ontology/> '+
+		         	'select ?subject ?predicate ?object ?frequency where { '+
+		         	'?pattern a ss:AbstractKnowledgePattern . ' +
+		         	'?pattern rdf:subject ?subject . ' +
+		         	'?pattern rdf:predicate ?predicate . ' + 
+		         	'?pattern rdf:object ?object . ' +
+		         	'?pattern ss:has_frequency ?frequency } ' +
+		         	'order by desc(?frequency) ' +
+		         	'limit 20')
+         	.onGraph($scope.selected_graph)
+         	.accumulate(function(results){
+				$scope.summaries=results;
+			});
+	};
+	
+	$scope.selected_graph='Select a dataset';
+	
+	new Sparql($http, $location)
+			.query("select distinct ?uri where {GRAPH ?uri {?s ?p ?o} . FILTER regex(?uri, 'schemasummaries')}")
+			.accumulate(function(results){
+				$scope.graphs=results;
+			});
+});
+
+Sparql = function(http_service, location_service){
+	
+	var http = http_service;
+	var location = location_service;
+	var graph = "";
+	var query;
+	
+	this.onGraph = function(target_graph){
+		graph = target_graph;
+		return this;
+	};
+	
+	this.query = function(query_to_execute){
+		query = query_to_execute;
+		return this;
+	};
+	
+	this.accumulate = function(onSuccess){
+		http.get('http://' + location.host() + ':8890/sparql', {
 	        method: 'GET',
 	        params: {
-	            query: 'prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ' +
-		               'prefix ss:   <http://schemasummaries.org/ontology/> '+
-	            	   'select ?subject ?predicate ?object ?frequency where { '+
-	            	   '?pattern a ss:AbstractKnowledgePattern . ' +
-	            	   '?pattern rdf:subject ?subject . ' +
-	            	   '?pattern rdf:predicate ?predicate . ' + 
-	            	   '?pattern rdf:object ?object . ' +
-	            	   '?pattern ss:has_frequency ?frequency } ' +
-	            	   'order by desc(?frequency) ' +
-	            	   'limit 20',
-	            'default-graph-uri': $scope.selected_graph,
+	            query: query,
+	            'default-graph-uri' : graph,
 	            format: 'json'
 	        }
 	    }).success(function(res){
-	        $scope.summaries=res.results.bindings;
+	    	onSuccess(res.results.bindings);
 	    });
-	}
-});
+	};
+};

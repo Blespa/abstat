a2cff6efe62b9493eda0242c9b17ff9cb2c25818
rporrini <riccardo.porrini@disco.unimib.it>
2015-03-09 22:40:13 +0100

    now the application goes always on the host that is requested

--- web/assets/js/controllers.js
+++ web/assets/js/controllers.js
@@ -1,13 +1,13 @@
 var summary = angular.module('schemasummaries', []);
 
-summary.controller('GraphsList', function ($scope, $http) {
-	$http.get('http://siti-rack.siti.disco.unimib.it:8890/sparql', {
+summary.controller('GraphsList', function ($scope, $http, $location) {
+	$http.get('http://' + $location.host() + ':8890/sparql', {
         method: 'GET',
         params: {
             query: "select distinct ?uri where {GRAPH ?uri {?s ?p ?o} . FILTER regex(?uri, 'schemasummaries')}",
             format: 'json'
         }
     }).success(function(res){
-        $scope.graphs=res.results.bindings
+        $scope.graphs=res.results.bindings;
     });
 });

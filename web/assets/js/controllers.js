67caf7550f878cb57083d3a603ab2499637c95a2
rporrini <riccardo.porrini@disco.unimib.it>
2015-03-09 23:53:51 +0100

    added a first implementation of the display of abstract patterns

--- web/assets/js/controllers.js
+++ web/assets/js/controllers.js
@@ -1,6 +1,8 @@
 var summary = angular.module('schemasummaries', []);
 
-summary.controller('GraphsList', function ($scope, $http, $location) {
+summary.controller('Summarization', function ($scope, $http, $location) {
+	$scope.selected_graph='Select a dataset'
+	
 	$http.get('http://' + $location.host() + ':8890/sparql', {
         method: 'GET',
         params: {
@@ -10,4 +12,26 @@ summary.controller('GraphsList', function ($scope, $http, $location) {
     }).success(function(res){
         $scope.graphs=res.results.bindings;
     });
+	
+	$scope.loadPatterns= function(){
+		$http.get('http://' + $location.host() + ':8890/sparql', {
+	        method: 'GET',
+	        params: {
+	            query: 'prefix rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ' +
+		               'prefix ss:   <http://schemasummaries.org/ontology/> '+
+	            	   'select ?subject ?predicate ?object ?frequency where { '+
+	            	   '?pattern a ss:AbstractKnowledgePattern . ' +
+	            	   '?pattern rdf:subject ?subject . ' +
+	            	   '?pattern rdf:predicate ?predicate . ' + 
+	            	   '?pattern rdf:object ?object . ' +
+	            	   '?pattern ss:has_frequency ?frequency } ' +
+	            	   'order by desc(?frequency) ' +
+	            	   'limit 20',
+	            'default-graph-uri': $scope.selected_graph,
+	            format: 'json'
+	        }
+	    }).success(function(res){
+	        $scope.summaries=res.results.bindings;
+	    });
+	}
 });

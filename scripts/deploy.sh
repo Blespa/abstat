4bf40cdda6cf55a246692b7eafd3adb0328552a3
rporrini <riccardo.porrini@disco.unimib.it>
2015-03-19 17:22:54 +0100

    typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-ssh -t $1 "export GIT_SSH=~/schema-summaries/scripts/git+ssh.sh && cd ~/schema-summaries && git checkout master && git pull && scripts/test-summarization-pipeline.sh && scripts/isql.sh 'SPARQL CLEAR GRAPH <http://ld-summaries.org/system-test>;' && scripts/build-java-ui-module.sh && sudo ln -s /home/schema-summaries/schema-summaries/scripts/java-ui-production.sh /etc/init.d/ld-summaries && sudo update-rc.d ld-summaries defaults && sudo service ld-summaries stop && sudo service ld-summaries start"
+ssh -t $1 "export GIT_SSH=~/schema-summaries/scripts/git+ssh.sh && cd ~/schema-summaries && git checkout master && git pull && scripts/test-summarization-pipeline.sh && scripts/isql.sh 'SPARQL CLEAR GRAPH <http://ld-summaries.org/system-test>;' && scripts/build-java-ui-module.sh && sudo rm -f /etc/init.d/ld-summaries && sudo ln -s /home/schema-summaries/schema-summaries/scripts/java-ui-production.sh /etc/init.d/ld-summaries && sudo update-rc.d ld-summaries defaults && sudo service ld-summaries stop && sudo service ld-summaries start"
 

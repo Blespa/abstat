f78a2c828e9f105b82373be3d9316ed70e45d3b8
rporrini <riccardo.porrini@disco.unimib.it>
2015-03-19 16:45:55 +0100

    polished the startup script

--- scripts/java-ui.sh
+++ scripts/java-ui.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+set -e
+
+function start(){
+	log_begin_msg "starting summarization-ui service on port $port"
+	start-stop-daemon --chuid $current_user --start --background --exec "/usr/bin/java" -m --pidfile "$pid" -d $project -- -cp .:"summarization-web.jar" it.unimib.disco.summarization.web.WebApplication $port
+	log_end_msg $?
+}
+
+function stop(){
+	log_begin_msg "stopping summarization-ui service on port $port"
+	start-stop-daemon --stop --pidfile "$pid"
+	log_end_msg $?
+	rm -f $pid
+}
+
 relative_path=`dirname $0`
 root=`cd $relative_path;pwd`
 project=$root/../web
@@ -17,19 +32,6 @@ pid=log/java-ui-$port.pid
 
 . /lib/lsb/init-functions
 
-function start(){
-	log_begin_msg "starting summarization-ui service on port $port"
-	start-stop-daemon --chuid $current_user --start --background --exec "/usr/bin/java" -m --pidfile "$pid" -d $project -- -cp .:"summarization-web.jar" it.unimib.disco.summarization.web.WebApplication $port
-	log_end_msg $?
-}
-
-function stop(){
-	log_begin_msg "stopping summarization-ui service on port $port"
-	start-stop-daemon --stop --pidfile "$pid"
-	log_end_msg $?
-	rm $pid
-}
-
 cd $project
 
 case "$1" in
@@ -41,8 +43,7 @@ case "$1" in
                 ;;
         *)
                 log_success_msg "Usage: java-ui.sh start|stop"
-		status=1
 		;;
 esac
-exit $status
+
 

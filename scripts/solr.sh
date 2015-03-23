8d603210fb5fb7151f8bcb93c96ce8b787c13bd7
TheEdoardo93 <edoardo.casiraghi.93@gmail.com>
2015-03-23 22:45:23 +0100

    tolti i commenti che non servono nello script "solr.sh". Solr up and running ok.

--- scripts/solr.sh
+++ scripts/solr.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-#function assert_application_is_up()
-#{
-#	url="localhost:$port/$page"
-#
-#	highlight_color='\e[0;31m'
-#	message='KO'
-#
-#	if [[ $(curl --silent $url | grep "Solr Admin") ]]
-#	then
-#		highlight_color='\e[0;32m'
-#		message="OK"
-#	fi
-#	echo -e "checking that $url is up: ${highlight_color}${message}\e[0m"
-#}
-
 function as_absolute()
 {
 	echo `cd $1; pwd`
@@ -26,15 +11,7 @@ current_directory=$(as_absolute $relative_path)
 port=$2
 page=$3
 
-#case "$1" in
-#        start)
-		$current_directory/../solr/bin/solr $1 -p $port
-#                ;;
-#	assert_application_is_up)
-#		assert_application_is_up
-#		;;
-#	stop)
-#		$current_directory/../solr/bin/solr stop -p $port
+$current_directory/../solr/bin/solr $1 -p $port
 
 echo
 

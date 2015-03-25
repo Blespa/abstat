d3722d72a7bbb9e436a1d67e620e79b50c55bdd8
rporrini <riccardo.porrini@disco.unimib.it>
2015-03-25 17:20:19 +0100

    now the script to create the yago1 dataset is more robust

--- scripts/export-all-datasets-to-rdf-on-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-ssh $1 "~/schema-summaries/scripts/export-to-rdf.sh dbpedia-2014-results schema-summaries/summarization-output http://ld-summaries.org/dbpedia-2014 && ~/schema-summaries/scripts/export-to-rdf.sh linked-brainz-results schema-summaries/summarization-output http://ld-summaries.org/linked-brainz"

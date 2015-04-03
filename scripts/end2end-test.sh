2fc346d62a2eaae0e5e337e85cd77c6401e99653
rporrini <riccardo.porrini@disco.unimib.it>
2015-04-03 11:27:54 +0200

    splitted the scripts for integration testing

--- scripts/end2end-test.sh
+++ scripts/end2end-test.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
 
-function assert_no_errors_on()
-{
-	file=$1
-	
-	highlight_color='\e[0;32m'
-	message='OK'
-	
-	if [[ $(grep "Command exited with non-zero status 1" $file) ]]
-	then
-		highlight_color='\e[0;31m'
-		message="KO. There were errors in log file ${file}"
-	fi
-	echo -e "checking that execution was successful: ${highlight_color}${message}\e[0m"
-}
-
-function assert_results_are_compliant()
-{
-	expected=$1
-	actual=$2
-	
-	highlight_color='\e[0;32m'
-	message='OK'
-	
-	if [[ $(diff -qr $expected $actual) ]]
-	then
-		highlight_color='\e[0;31m'
-		message="KO"
-	fi
-	echo -e "checking that execution produced the correct result: ${highlight_color}${message}"
-	echo $(diff -qr $expected $actual)	
-	echo -e "\e[0m"
-}
-
 function assert_results_are_present_in_virtuoso(){
 	sparql_query="http://localhost:8890/sparql?default-graph-uri=http%3A%2F%2Fld-summaries.org%2Fsystem-test&query=select+count%28*%29+where+%7B%3Fa+%3Fb+%3Fc%7D&format=text%2Fplain&timeout=0&debug=on"
 	expected="<http://www.w3.org/2005/sparql-results#value> \"6549\"^^<http://www.w3.org/2001/XMLSchema#integer>"
@@ -74,10 +41,8 @@ set -e
 relative_path=`dirname $0`
 current_directory=$(as_absolute $relative_path)
 root=$(as_absolute $current_directory/../)
-data=$root/benchmark/regression-test
-results=$root/benchmark/tmp
-expected_results=$root/benchmark/regression-test-results
 rdf_export_path=$root/summarization-output
+results=$root/benchmark/tmp
 
 echo
 echo "SYSTEM TEST"
@@ -125,20 +90,17 @@ then
 fi
 echo
 
-mkdir -p $expected_results/patterns/tmp-files
-
 cd $current_directory
-./test-java-ui-module.sh
-./test-java-summarization-module.sh
-./run-summarization-pipeline.sh $data $results
-echo
-assert_no_errors_on ../summarization/log/log.txt
-assert_results_are_compliant $expected_results $results
+
+./test-summarization-pipeline.sh
 
 graph=http://ld-summaries.org/system-test
 ./export-to-rdf.sh $results $rdf_export_path $graph
 echo
 assert_results_are_present_in_virtuoso
+
+echo
+./test-java-ui-module.sh
 echo
 
 echo "integration testing of the web interface module"
---
+++ scripts/test-summarization-pipeline.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+function assert_no_errors_on()
+{
+	file=$1
+	
+	highlight_color='\e[0;32m'
+	message='OK'
+	
+	if [[ $(grep "Command exited with non-zero status 1" $file) ]]
+	then
+		highlight_color='\e[0;31m'
+		message="KO. There were errors in log file ${file}"
+	fi
+	echo -e "checking that execution was successful: ${highlight_color}${message}\e[0m"
+}
+
+function assert_results_are_compliant()
+{
+	expected=$1
+	actual=$2
+	
+	highlight_color='\e[0;32m'
+	message='OK'
+	
+	if [[ $(diff -qr $expected $actual) ]]
+	then
+		highlight_color='\e[0;31m'
+		message="KO"
+	fi
+	echo -e "checking that execution produced the correct result: ${highlight_color}${message}"
+	echo $(diff -qr $expected $actual)	
+	echo -e "\e[0m"
+}
+
+function as_absolute(){
+	echo `cd $1; pwd`
+}
+
+set -e
+relative_path=`dirname $0`
+current_directory=$(as_absolute $relative_path)
+root=$(as_absolute $current_directory/../)
+data=$root/benchmark/regression-test
+results=$root/benchmark/tmp
+expected_results=$root/benchmark/regression-test-results
+
+mkdir -p $expected_results/patterns/tmp-files
+
+cd $current_directory
+./test-java-summarization-module.sh
+./run-summarization-pipeline.sh $data $results
+echo
+assert_no_errors_on ../summarization/log/log.txt
+assert_results_are_compliant $expected_results $results
+
+echo
+

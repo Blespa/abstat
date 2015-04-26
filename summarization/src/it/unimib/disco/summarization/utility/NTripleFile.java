4d4a4f5b55f636a84479437595b99a504c907530
rporrini <riccardo.porrini@disco.unimib.it>
2015-04-26 22:48:48 +0200

    now we have a ntriple processor

--- summarization/src/it/unimib/disco/summarization/utility/NTripleFile.java
+++ summarization/src/it/unimib/disco/summarization/utility/NTripleFile.java
@@ -1,13 +1,41 @@
 package it.unimib.disco.summarization.utility;
 
+import it.unimib.disco.summarization.starter.Events;
+
 import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.LineIterator;
+import org.semanticweb.yars.nx.parser.NxParser;
 
 public class NTripleFile {
 
+	private static final Pattern isAcceptable = Pattern.compile("^<.+> <.+> (?<object>.+) \\.");
+	private NTripleAnalysis analysis;
+
 	public NTripleFile(NTripleAnalysis analysis) {
+		this.analysis = analysis;
 	}
 
-	public void process(File file) {
+	public void process(File file) throws Exception {
+		TextInput input = new TextInput(new FileSystemConnector(file));
+		while(input.hasNextLine()){
+			String line = input.nextLine();
+			Matcher matcher = isAcceptable.matcher(line);
+			if(!matcher.matches()){
+				continue;
+			}
+			String rawObject = matcher.group("object");
+			if(rawObject.startsWith("<")) line = line.replace(rawObject, rawObject.replace(" ", "%20"));
+			try{
+				NTriple triple = new NTriple(NxParser.parseNodes(line));
+				analysis.track(triple);
+			}catch(Exception e){
+				new Events().error("error processing " + input.name(), e);
+			}
+		}
 	}
 
 }
--- summarization/test/it/unimib/disco/summarization/tests/NTripleFileTest.java
+++ summarization/test/it/unimib/disco/summarization/tests/NTripleFileTest.java
@@ -17,4 +17,47 @@ public class NTripleFileTest extends TestWithTemporaryData{
 		
 		assertThat(analysis.countProcessed(), equalTo(0));
 	}
+	
+	@Test
+	public void shouldAnalyzeAWellFormedNTripleFile() throws Exception {
+		
+		NTripleAnalysisInspector analysis = new NTripleAnalysisInspector();
+		
+		new NTripleFile(analysis).process(temporary.file("<a> <b> <c> ."));
+		
+		assertThat(analysis.countProcessed(), equalTo(1));
+	}
+	
+	@Test(timeout = 1000)
+	public void shouldSkipStrangeLines() throws Exception {
+		NTripleAnalysisInspector analysis = new NTripleAnalysisInspector();
+		
+		new NTripleFile(analysis)
+				.process(temporary.file("曲：[http://musicbrainz.org/artist/a223958d-5c56-4b2c-a30a-87e357bc121b.html|周杰倫]"));
+	
+		assertThat(analysis.countProcessed(), equalTo(0));
+	}
+	
+	@Test(timeout = 1000)
+	public void shouldSkipOtherStrangeLines() throws Exception {
+		NTripleAnalysisInspector analysis = new NTripleAnalysisInspector();
+		
+		new NTripleFile(analysis)
+				.process(temporary.file(
+						"Inaccurate ARs:" + "\n" +
+						"" + "\n" +
+						"    * 混音助理 means \"mixing assistant\", but is credited as \"co-mixer\".\" ."
+						));
+	
+		assertThat(analysis.countProcessed(), equalTo(0));
+	}
+	
+	@Test
+	public void shouldIndexAlsoWithSpaces() throws Exception {
+		NTripleAnalysisInspector analysis = new NTripleAnalysisInspector();
+		
+		new NTripleFile(analysis).process(temporary.file("<http://1234> <http://predicate> <http://uri with space> ."));
+		
+		assertThat(analysis.countProcessed(), equalTo(1));
+	}
 }

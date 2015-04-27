4fba24de1ba04af5e9e6f0cb2efd6a56373cfd73
rporrini <riccardo.porrini@disco.unimib.it>
2015-04-27 15:55:11 +0200

    added the implementation for counting the properties

---
+++ summarization/src/it/unimib/disco/summarization/utility/PropertyCount.java
@@ -0,0 +1,23 @@
+package it.unimib.disco.summarization.utility;
+
+import java.util.HashMap;
+
+public class PropertyCount {
+
+	private HashMap<String, Long> counts;
+
+	public PropertyCount() {
+		counts = new HashMap<String, Long>();
+	}
+	
+	public HashMap<String, Long> counts() {
+		return counts;
+	}
+	
+	public PropertyCount track(NTriple triple) {
+		String property = triple.property().asResource().getURI();
+		if(!counts.containsKey(property)) counts.put(property, 0l);
+		counts.put(property, counts.get(property) + 1);
+		return this;
+	}
+}

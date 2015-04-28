af685312a612ae2413b4718499bc60e27858aeb3
rporrini <riccardo.porrini@disco.unimib.it>
2015-04-29 01:15:16 +0200

    stats

--- summarization/src/it/unimib/disco/summarization/experiments/PatternStatistics.java
+++ summarization/src/it/unimib/disco/summarization/experiments/PatternStatistics.java
@@ -4,12 +4,14 @@ import it.unimib.disco.summarization.output.Events;
 import it.unimib.disco.summarization.output.LDSummariesVocabulary;
 
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.vocabulary.RDFS;
 
 public class PatternStatistics {
 
 	public static void main(String[] args) {
 		
-		String dataset = args[0];
+		String dataset = "linked-brainz";
+		String ontology = "http://purl.org/ontology/mo/";
 		
 		new Events();
 		
@@ -22,6 +24,24 @@ public class PatternStatistics {
 					 		 	+ "?pattern a <"+ vocabulary.abstractKnowledgePattern().getURI() + "> ." +
 					 		  "}")
 					 	.next().getLiteral("count").getInt();
+//		int internalAKP = new AbstatEndpoint()
+//						.execute("select (count(?pattern) as ?count)"
+//							 + "from <" + vocabulary.graph() + "> "
+//							 + "where {"
+//						 		+ "?pattern a <"+ vocabulary.abstractKnowledgePattern().getURI() + "> ."
+//							 	+ "?pattern <"+ vocabulary.subject()+ "> ?localSubject ."
+//							 	+ "?localSubject <"+ RDFS.seeAlso + "> ?subject ."
+//							 	+ "?pattern <"+ vocabulary.object()+ "> ?localObject ."
+//							 	+ "?localObject a <"+ vocabulary.type() + "> . "
+//							 	+ "?localObject <"+ RDFS.seeAlso + "> ?object . "
+//							 	+ "filter not exists {"
+//							 		+ "?localObject a <" + vocabulary.datatype() + "> . "
+//						 		+ "} "
+//							 	+ "filter regex(?subject, '" + ontology + "') "
+//							 	+ "filter regex(?object, '" + ontology + "') "
+//							 + "}")
+//					 	.next().getLiteral("count").getInt();
+		
 		int datatypeAKP = new AbstatEndpoint()
 						.execute("select (count(?pattern) as ?count)"
 							 + "from <" + vocabulary.graph() + "> "
@@ -30,7 +50,7 @@ public class PatternStatistics {
 					 		 	+ "?pattern <"+ vocabulary.object() + "> ?object . "
 					 		 	+ "?object a <"+ vocabulary.datatype() + "> . "
 					 		 + "}")
-	 	.next().getLiteral("count").getInt();
+			 		 	.next().getLiteral("count").getInt();
 		
 		System.out.println("AKP:\t" + totalAKP);
 		System.out.println("Object AKP:\t" + (totalAKP - datatypeAKP));

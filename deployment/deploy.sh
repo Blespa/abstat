8bd534ee50083685fdd69daa0edd3e39073fc262
rporrini <riccardo.porrini@disco.unimib.it>
2015-05-20 14:45:38 +0200

    corrected typo

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-ssh -t $1 "export GIT_SSH=/schema-summaries/deployment/git+ssh.sh && cd /schema-summaries && git checkout master && git pull && ./abstat.sh build && ./abstat.sh destroy && ./abstat.sh start"
+ssh -t $1 "export GIT_SSH=~/schema-summaries/deployment/git+ssh.sh && cd /schema-summaries && git checkout master && git pull && ./abstat.sh build && ./abstat.sh destroy && ./abstat.sh start"
 

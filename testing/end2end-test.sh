586573e315f57d19ef2806b21b5b801fd2b19162
rporrini <riccardo.porrini@disco.unimib.it>
2015-05-21 16:32:09 +0200

    more simplifications

--- testing/end2end-test.sh
+++ testing/end2end-test.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-function assert_results_are_present_in_virtuoso(){
-	sparql_query="http://localhost/sparql?default-graph-uri=http%3A%2F%2Fld-summaries.org%2Fsystem-test&query=select+count%28*%29+where+%7B%3Fa+%3Fb+%3Fc%7D&format=text%2Fplain&timeout=0&debug=on"
-	expected="<http://www.w3.org/2005/sparql-results#value> \"3858\"^^<http://www.w3.org/2001/XMLSchema#integer>"
-
-	highlight_color='\e[0;31m'
-	message='KO'
-	if [[ $(curl --silent "$sparql_query" | grep "$expected") ]]
-	then
-		highlight_color='\e[0;32m'
-		message="OK"
-	fi
-	echo -e "checking that rdf produced was loaded: ${highlight_color}${message}\e[0m"
-}
-
 function as_absolute(){
 	echo `cd $1; pwd`
 }
@@ -39,23 +25,13 @@ function assert_application_is_up(){
 set -e
 relative_path=`dirname $0`
 current_directory=$(as_absolute $relative_path)
-root=$(as_absolute $current_directory/../)
-rdf_export_path=$root/summarization-output
 
 cd $current_directory
-
 echo "SYSTEM TEST"
 ./check-system-configuration.sh
-
-./test-summarization-pipeline.sh
-
-graph=http://ld-summaries.org/system-test
-cd ../scripts
-./export-to-rdf.sh system-test $rdf_export_path $graph
-cd ../testing
-assert_results_are_present_in_virtuoso
-
 ./test-java-ui-module.sh
+./test-java-summarization-module.sh
+./test-summarization-pipeline.sh
 
 echo "integration testing of the web interface module"
 assert_application_is_up '/' 'ABSTAT'
--- testing/test-summarization-pipeline.sh
+++ testing/test-summarization-pipeline.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+function assert_results_are_present_in_virtuoso(){
+	sparql_query="http://localhost/sparql?default-graph-uri=http%3A%2F%2Fld-summaries.org%2Fsystem-test&query=select+count%28*%29+where+%7B%3Fa+%3Fb+%3Fc%7D&format=text%2Fplain&timeout=0&debug=on"
+	expected="<http://www.w3.org/2005/sparql-results#value> \"3858\"^^<http://www.w3.org/2001/XMLSchema#integer>"
+
+	highlight_color='\e[0;31m'
+	message='KO'
+	if [[ $(curl --silent "$sparql_query" | grep "$expected") ]]
+	then
+		highlight_color='\e[0;32m'
+		message="OK"
+	fi
+	echo -e "checking that rdf produced was loaded: ${highlight_color}${message}\e[0m"
+}
+
 function assert_no_errors_on()
 {
 	file=$1
@@ -49,9 +63,11 @@ mkdir -p $expected_results/patterns/tmp-files
 rm -rf $results
 
 cd $current_directory
-./test-java-summarization-module.sh
 cd ../scripts
 ./run-summarization-pipeline.sh system-test
 assert_no_errors_on ../summarization/log/log.txt
 assert_results_are_compliant $expected_results $results
+./export-to-rdf.sh system-test ../summarization-output http://ld-summaries.org/system-test
+assert_results_are_present_in_virtuoso
 cd ../testing
+

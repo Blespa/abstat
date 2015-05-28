8190215ee99c5307e08febf36ebc20e3a58a63e1
rporrini <riccardo.porrini@disco.unimib.it>
2015-05-28 18:18:15 +0200

    fix

--- datasets/prepare-dbpedia-labelling-dataset.sh
+++ datasets/prepare-dbpedia-labelling-dataset.sh
@@ -6,7 +6,7 @@ version=$1
 include_raw=$2
 dbpedia_downloads="http://downloads.dbpedia.org/${version}"
 
-target_directory=/schema-summaries/data/datasets/labelling-dbpedia-$1{$include_raw}
+target_directory="/schema-summaries/data/datasets/labelling-dbpedia-${version}${include_raw}"
 rm -rf $target_directory
 mkdir -p $target_directory
 

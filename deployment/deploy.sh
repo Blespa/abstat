da877104895f0683ca3862924cae8114cf1a3f1c
rporrini <riccardo.porrini@disco.unimib.it>
2015-05-28 16:06:13 +0200

    deployment

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-ssh -t $1 "export GIT_SSH=~/schema-summaries/deployment/git+ssh.sh && cd ~/schema-summaries && git remote set-url origin git@bitbucket.org:rporrini/schema-summaries.git && chmod 700 deployment/deploy_rsa && git pull && git checkout -f master && ./install.sh $2"
+ssh -o "StrictHostKeyChecking no" -t $1 "export GIT_SSH=~/schema-summaries/deployment/git+ssh.sh && cd ~/schema-summaries && git remote set-url origin git@bitbucket.org:rporrini/schema-summaries.git && chmod 700 deployment/deploy_rsa && git pull && git checkout -f master && ./install.sh $2"
 

47c5732f6b70a0cf6019c1429073283616bf0324
rporrini <riccardo.porrini@disco.unimib.it>
2015-06-03 14:53:12 +0200

    moved the system tests

---
+++ summarization/test/it/unimib/disco/summarization/systemTests/SummarizationPipelineTest.java
@@ -0,0 +1,13 @@
+package it.unimib.disco.summarization.systemTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SummarizationPipelineTest {
+
+	@Test
+	public void shouldDistinguishBetweenDatatypeAndObjectProperties() {
+		
+	}
+}
--- testing/system-tests.sh
+++ testing/system-tests.sh
@@ -3,10 +3,10 @@
 set -e
 relative_path=`dirname $0`
 root=`cd $relative_path;pwd`
-project=$root/../system-tests
+project=$root/../summarization
 
 echo running the system tests
 cd $project
-java -Xms256m -Xmx1g -cp .:'system-tests.jar' org.junit.runner.JUnitCore it.unimib.disco.summarization.systemTests.TestSuite
+java -Xms256m -Xmx1g -cp .:'ontology_summarization.jar' org.junit.runner.JUnitCore it.unimib.disco.summarization.systemTests.TestSuite
 cd $root
 

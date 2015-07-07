3e6d30626309cb73438f842e8d5ac1c6b533b9fa
rporrini <riccardo.porrini@disco.unimib.it>
2015-07-07 14:55:17 +0200

    refactoring

--- summarization/test/it/unimib/disco/summarization/systemTests/HostsTest.java
+++ summarization/test/it/unimib/disco/summarization/systemTests/HostsTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import org.junit.Test;
 
@@ -12,12 +13,16 @@ public class HostsTest {
 	@Test
 	public void backendIsReachable() throws Exception {
 		
-		assertThat(InetAddress.getByName("backend").getHostAddress(), equalTo("193.204.59.21"));
+		assertThat(addressOf("backend"), equalTo("193.204.59.21"));
 	}
-	
+
 	@Test
 	public void frontendIsReachable() throws Exception {
 		
-		assertThat(InetAddress.getByName("frontend").getHostAddress(), equalTo("10.109.149.57"));
+		assertThat(addressOf("frontend"), equalTo("10.109.149.57"));
+	}
+	
+	private String addressOf(String host) throws UnknownHostException {
+		return InetAddress.getByName(host).getHostAddress();
 	}
 }
--- summarization/test/it/unimib/disco/summarization/systemTests/SolrIndexingTest.java
+++ summarization/test/it/unimib/disco/summarization/systemTests/SolrIndexingTest.java
@@ -1,47 +1,38 @@
 package it.unimib.disco.summarization.systemTests;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.Assert.assertThat;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.junit.Test;
 
 public class SolrIndexingTest {
 	
 	@Test
 	public void solrConceptsIndexingShouldBeOk() throws Exception {
-		assertThat(httpResponseFrom("http://localhost/solr/indexing/select?q=type:concept"), containsString("numFound=\"20\""));
+		HttpAssert.body("http://localhost/solr/indexing/select?q=type:concept", containsString("numFound=\"20\""));
 	}
 	
 	@Test
 	public void solrDatatypePropertiesIndexingShouldBeOk() throws Exception {
-		assertThat(httpResponseFrom("http://localhost/solr/indexing/select?q=type:datatypeProperty"), containsString("numFound=\"11\""));
+		HttpAssert.body("http://localhost/solr/indexing/select?q=type:datatypeProperty", containsString("numFound=\"11\""));
 	}
 	
 	@Test
 	public void solrObjectPropertiesIndexingShouldBeOk() throws Exception {
-		assertThat(httpResponseFrom("http://localhost/solr/indexing/select?q=type:objectProperty"), containsString("numFound=\"5\""));
+		HttpAssert.body("http://localhost/solr/indexing/select?q=type:objectProperty", containsString("numFound=\"5\""));
 	}
 	
 	@Test
 	public void solrDatatypeAkpsIndexingShouldBeOk() throws Exception {
-		assertThat(httpResponseFrom("http://localhost/solr/indexing/select?q=type:datatypeAkp"), containsString("numFound=\"68\""));
+		HttpAssert.body("http://localhost/solr/indexing/select?q=type:datatypeAkp", containsString("numFound=\"68\""));
 	}
 	
 	@Test
 	public void solrObjectAkpsIndexingShouldBeOk() throws Exception {
-		assertThat(httpResponseFrom("http://localhost/solr/indexing/select?q=type:objectAkp"), containsString("numFound=\"109\""));
+		HttpAssert.body("http://localhost/solr/indexing/select?q=type:objectAkp", containsString("numFound=\"109\""));
 	}
 	
 	@Test
 	public void solrIndexingShouldBeOk() throws Exception {
-		assertThat(httpResponseFrom("http://localhost/solr/indexing/select?q=*:*"), containsString("numFound=\"213\""));
-	}
-	
-	private String httpResponseFrom(String address) throws Exception{
-		return StringUtils.join(IOUtils.readLines(new DefaultHttpClient().execute(new HttpGet(address)).getEntity().getContent()), "\n");
+		HttpAssert.body("http://localhost/solr/indexing/select?q=*:*", containsString("numFound=\"213\""));
 	}
 }

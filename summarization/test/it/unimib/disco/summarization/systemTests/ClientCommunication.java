6aa186e2ee958145d8e1efa2aaf7f9919c902d5e
rporrini <riccardo.porrini@disco.unimib.it>
2015-07-08 13:27:30 +0200

    now we log also the sessions

--- summarization/test/it/unimib/disco/summarization/systemTests/ClientCommunication.java
+++ summarization/test/it/unimib/disco/summarization/systemTests/ClientCommunication.java
@@ -11,6 +11,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
 
 public class ClientCommunication{
 	
@@ -21,6 +22,7 @@ public class ClientCommunication{
 		this.host = host;
 		HttpParams connection = new BasicHttpParams();
 		HttpConnectionParams.setConnectionTimeout(connection, 2000);
+		HttpConnectionParams.setSoKeepalive(connection, true);
 		this.client = new DefaultHttpClient(connection);
 	}
 	
@@ -31,7 +33,15 @@ public class ClientCommunication{
 	}	
 	
 	public List<Cookie> getCookiesFrom(String path) throws Exception{
-		httpGet(path);
+		EntityUtils.consume(httpGet(path).getEntity());
 		return client.getCookieStore().getCookies();
+	}
+	
+	public Cookie getCookie(String path, String id) throws Exception{
+		Cookie result = null;
+		for(Cookie cookie : getCookiesFrom(path)){
+			if(cookie.getName().equals(id)) result = cookie;
+		}
+		return result;
 	}
 }

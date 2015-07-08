055366d03f02fd7b47fe87a8aa8cede770b978dd
rporrini <riccardo.porrini@disco.unimib.it>
2015-07-08 12:47:46 +0200

    now we track sessions

--- web/test/it/unimib/disco/summarization/web/tests/ClientCommunication.java
+++ web/test/it/unimib/disco/summarization/web/tests/ClientCommunication.java
@@ -1,10 +1,12 @@
 package it.unimib.disco.summarization.web.tests;
 
 import java.net.URI;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
@@ -13,16 +15,23 @@ import org.apache.http.params.HttpParams;
 public class ClientCommunication{
 	
 	private String host;
+	private DefaultHttpClient client;
 
 	public ClientCommunication(String host){
 		this.host = host;
+		HttpParams connection = new BasicHttpParams();
+		HttpConnectionParams.setConnectionTimeout(connection, 2000);
+		this.client = new DefaultHttpClient(connection);
 	}
 	
 	public HttpResponse httpGet(String path) throws Exception{
 		HttpRequestBase request = new HttpGet();
 		request.setURI(new URI(host + "/" + path));
-		HttpParams connection = new BasicHttpParams();
-		HttpConnectionParams.setConnectionTimeout(connection, 2000);
-		return new DefaultHttpClient(connection).execute(request);
+		return client.execute(request);
+	}	
+	
+	public List<Cookie> getCookiesFrom(String path) throws Exception{
+		httpGet(path);
+		return client.getCookieStore().getCookies();
 	}
 }

292bd2e4875668ca5b341cf2664c516eee021f2c
rporrini <riccardo.porrini@disco.unimib.it>
2015-07-08 14:14:11 +0200

    refinement

--- summarization/test/it/unimib/disco/summarization/systemTests/SolrIndexingTest.java
+++ summarization/test/it/unimib/disco/summarization/systemTests/SolrIndexingTest.java
@@ -8,32 +8,32 @@ public class SolrIndexingTest {
 	
 	@Test
 	public void solrConceptsIndexingShouldBeOk() throws Exception {
-		httpAssert().body("/select?q=type:concept", containsString("numFound=\"20\""));
+		httpAssert().body("select?q=type:concept", containsString("numFound=\"20\""));
 	}
 
 	@Test
 	public void solrDatatypePropertiesIndexingShouldBeOk() throws Exception {
-		httpAssert().body("/select?q=type:datatypeProperty", containsString("numFound=\"11\""));
+		httpAssert().body("select?q=type:datatypeProperty", containsString("numFound=\"11\""));
 	}
 	
 	@Test
 	public void solrObjectPropertiesIndexingShouldBeOk() throws Exception {
-		httpAssert().body("/select?q=type:objectProperty", containsString("numFound=\"5\""));
+		httpAssert().body("select?q=type:objectProperty", containsString("numFound=\"5\""));
 	}
 	
 	@Test
 	public void solrDatatypeAkpsIndexingShouldBeOk() throws Exception {
-		httpAssert().body("/select?q=type:datatypeAkp", containsString("numFound=\"68\""));
+		httpAssert().body("select?q=type:datatypeAkp", containsString("numFound=\"68\""));
 	}
 	
 	@Test
 	public void solrObjectAkpsIndexingShouldBeOk() throws Exception {
-		httpAssert().body("/select?q=type:objectAkp", containsString("numFound=\"109\""));
+		httpAssert().body("select?q=type:objectAkp", containsString("numFound=\"109\""));
 	}
 	
 	@Test
 	public void solrIndexingShouldBeOk() throws Exception {
-		httpAssert().body("/select?q=*:*", containsString("numFound=\"213\""));
+		httpAssert().body("select?q=*:*", containsString("numFound=\"213\""));
 	}
 	
 	private HttpAssert httpAssert() {

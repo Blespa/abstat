ef4ede1e575721f8db76c3fc4cd223d530f15f73
TheEdoardo93 <edoardo.casiraghi.93@gmail.com>
2015-07-15 18:26:14 +0200

    TASK #52:
            - Passato come primo argomento dello script 'test-indexing-pipeline.sh' il nome del dataset cui si vuole indicizzare quando si richiama questo script a partire$
            - Cancellare tutti i documenti che fanno parte di quel dataset passato come unico parametro nel file 'test-indexing-pipeline.sh';
            - Nel file 'test-indexing-pipeline.sh' aggiunta come variabile 'dataset=$1', vengono cambiati il nome della cartella che prima era 'system-test' nella variabil$
    
    OK.

--- testing/test-indexing-pipeline.sh
+++ testing/test-indexing-pipeline.sh
@@ -10,6 +10,8 @@ relative_path=`dirname $0`
 current_directory=$(as_absolute $relative_path)
 cd $current_directory
 
+dataset=$1
+
 echo
 echo "indexing the produced summary"
 
@@ -17,28 +19,28 @@ solr_port=8891
 
 #Delete all the documents that are in the index.
 cd ../summarization
-java -Xms256m -Xmx4g -cp .:'ontology_summarization.jar' it.unimib.disco.summarization.output.DeleteAllDocumentsIntoIndex localhost $solr_port system-test
+java -Xms256m -Xmx4g -cp .:'ontology_summarization.jar' it.unimib.disco.summarization.output.DeleteAllDocumentsIntoIndex localhost $solr_port $dataset
 cd ../testing
 
 sleep 1
 
 cd ../summarization
-java -Xms256m -Xmx4g -cp .:'ontology_summarization.jar' it.unimib.disco.summarization.output.IndexConcepts localhost $solr_port ../data/summaries/system-test/patterns/count-concepts.txt system-test
+java -Xms256m -Xmx4g -cp .:'ontology_summarization.jar' it.unimib.disco.summarization.output.IndexConcepts localhost $solr_port ../data/summaries/$dataset/patterns/count-concepts.txt $dataset
 cd ../testing
 
 cd ../summarization
-java -Xms256m -Xmx4g -cp .:'ontology_summarization.jar' it.unimib.disco.summarization.output.IndexDatatypeProperties localhost $solr_port ../data/summaries/system-test/patterns/count-datatype-properties.txt system-test
+java -Xms256m -Xmx4g -cp .:'ontology_summarization.jar' it.unimib.disco.summarization.output.IndexDatatypeProperties localhost $solr_port ../data/summaries/$dataset/patterns/count-datatype-properties.txt $dataset
 cd ../testing
 
 cd ../summarization
-java -Xms256m -Xmx4g -cp .:'ontology_summarization.jar' it.unimib.disco.summarization.output.IndexObjectProperties localhost $solr_port ../data/summaries/system-test/patterns/count-object-properties.txt system-test
+java -Xms256m -Xmx4g -cp .:'ontology_summarization.jar' it.unimib.disco.summarization.output.IndexObjectProperties localhost $solr_port ../data/summaries/$dataset/patterns/count-object-properties.txt $dataset
 cd ../testing
 
 cd ../summarization
-java -Xms256m -Xmx4g -cp .:'ontology_summarization.jar' it.unimib.disco.summarization.output.IndexDatatypeAkps localhost $solr_port ../data/summaries/system-test/patterns/datatype-akp.txt system-test
+java -Xms256m -Xmx4g -cp .:'ontology_summarization.jar' it.unimib.disco.summarization.output.IndexDatatypeAkps localhost $solr_port ../data/summaries/$dataset/patterns/datatype-akp.txt $dataset
 cd ../testing
 
 cd ../summarization
-java -Xms256m -Xmx4g -cp .:'ontology_summarization.jar' it.unimib.disco.summarization.output.IndexObjectAkps localhost $solr_port ../data/summaries/system-test/patterns/object-akp.txt system-test
+java -Xms256m -Xmx4g -cp .:'ontology_summarization.jar' it.unimib.disco.summarization.output.IndexObjectAkps localhost $solr_port ../data/summaries/$dataset/patterns/object-akp.txt $dataset
 cd ../testing
 

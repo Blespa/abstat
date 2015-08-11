121ddfac671b0481049944cff451ff55cfaf7ba2
rporrini <riccardo.porrini@disco.unimib.it>
2015-08-11 12:50:57 +0200

    ui refinement

--- web/assets/js/search.js
+++ web/assets/js/search.js
@@ -1,5 +1,32 @@
 var application = angular.module("schemasummaries", []);
 
+application.filter('describe', function(){
+	return function(objectToDescribe){
+		return objectToDescribe.join(' - ');
+	};
+});
+
+application.filter('clean', function(){
+	return function(type){
+		return type.toLowerCase().replace(/object/g, "").replace(/datatype/g, "");
+	};
+});
+
+application.filter('asLabel', function(){
+	return function(type){
+		if(type == 'concept') return 'success';
+		if(type == 'property') return 'danger';
+		if(type == 'akp') return 'warning';
+	};
+});
+
+application.filter('asIcon', function(){
+	return function(subtype){
+		if(subtype.indexOf('external') > -1) return 'full';
+		return 'small'
+	};
+});
+
 application.controller("search", function ($scope, $http) {
 	
 	escape = function(string){
@@ -12,7 +39,8 @@ application.controller("search", function ($scope, $http) {
 			method: 'GET',
 			params: {
 				wt: 'json',
-				q: 'fullTextSearchField:' + escape($scope.srcStr)
+				q: 'fullTextSearchField:' + escape($scope.srcStr),
+				rows: 10000
 			}
 		}).success(function(results){
 			$scope.allDocuments = results.response.docs;

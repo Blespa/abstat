ac72f9b0c13e425d33417c01dd28b099e64dd5a1
rporrini <riccardo.porrini@disco.unimib.it>
2015-09-25 11:14:53 +0200

    now the scripts are more simple

--- deployment/act-as-binary.sh
+++ deployment/act-as-binary.sh
@@ -2,18 +2,24 @@
 
 set -e
 
+virtuoso='/usr/bin/virtuoso-t +wait -c deployment/virtuoso.ini'
+
 if [[ $1 != --dry ]]
 then
-	mkdir -p /schema-summaries/data/logs/webapp
-	mkdir -p /schema-summaries/data/logs/reverse-proxy
-	mkdir -p /schema-summaries/data/logs/summarization
-	mkdir -p /schema-summaries/data/logs/solr
+	mkdir -p data/logs/webapp
+	mkdir -p data/logs/reverse-proxy
+	mkdir -p data/logs/summarization
+	mkdir -p data/logs/solr
+
+	cp deployment/nginx.conf /etc/nginx/
+	cp deployment/fct_dav.vad /usr/share/virtuoso-opensource-7/vad/ && chmod 644 /usr/share/virtuoso-opensource-7/vad/fct_dav.vad
+
 	echo "starting up all services"
-	./java-ui.sh start 8892
-	./solr.sh start 8891
+	deployment/java-ui.sh start 8892
+	deployment/solr.sh start 8891
 	nginx
-	/usr/bin/virtuoso-t +wait && /usr/bin/isql-vt 1111 dba dba "EXEC=vad_install ('/usr/share/virtuoso-opensource-7/vad/fct_dav.vad', 0);" && /usr/bin/isql-vt 1111 dba dba "EXEC=SHUTDOWN;"
-	/usr/bin/virtuoso-t +wait
+	$virtuoso && pipeline/isql.sh "vad_install ('/usr/share/virtuoso-opensource-7/vad/fct_dav.vad', 0);" && pipeline/isql.sh "SHUTDOWN;"
+	$virtuoso
 	echo "all services started up"
 fi
 

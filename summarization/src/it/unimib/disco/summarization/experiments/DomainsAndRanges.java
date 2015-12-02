273d7a9a41714d3d19d1ee6b5034d836cd19d09b
rporrini <r.porrini@gmail.com>
2015-12-02 10:21:46 +0100

    now we print also internal and external

--- summarization/src/it/unimib/disco/summarization/experiments/DomainsAndRanges.java
+++ summarization/src/it/unimib/disco/summarization/experiments/DomainsAndRanges.java
@@ -1,6 +1,7 @@
 package it.unimib.disco.summarization.experiments;
 
 import it.unimib.disco.summarization.ontology.LDSummariesVocabulary;
+import it.unimib.disco.summarization.ontology.TypeOf;
 
 import java.util.List;
 
@@ -11,14 +12,23 @@ public class DomainsAndRanges {
 
 	public static void main(String[] args) {
 		String dataset = "dbpedia-2014";
+		String domain = "dbpedia.org/ontology";
 		
 		LDSummariesVocabulary vocabulary = new LDSummariesVocabulary(ModelFactory.createDefaultModel(), dataset);
 		SparqlEndpoint endpoint = SparqlEndpoint.abstat();
+		TypeOf classifier = new TypeOf(domain);
 		
 		List<Resource[]> allProperties = new SummarizedProperties(vocabulary, endpoint).all();
 		
 		for(Resource[] property : allProperties){
-			System.out.println("\"" + property[1] + "\"");
+			String uri = property[1].toString();
+			String type = classifier.resource(property[0].toString());
+			
+			System.out.println(escaped(uri) + "\t" + escaped(type));
 		}
+	}
+
+	private static String escaped(String uri) {
+		return "\"" + uri + "\"";
 	}
 }

663f10768a7681603f1b4f2a113eebbc968f61d2
Riccardo Porrini <you@example.com>
2015-12-22 12:00:05 +0100

    fix per pull request

--- summarization/src/it/unimib/disco/summarization/web/WebApplication.java
+++ summarization/src/it/unimib/disco/summarization/web/WebApplication.java
@@ -7,7 +7,6 @@ public class WebApplication {
 	public static void main(String[] args) throws Exception {
 		try{
 			new SummarizationBrowser().on(Integer.parseInt(args[0])).start();
-			//new SummarizationBrowser().on(8888).start();
 		}
 		catch(Exception e){
 			Events.web().error("application didn't start", e);

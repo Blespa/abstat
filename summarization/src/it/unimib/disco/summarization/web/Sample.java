e5a30f57d4edf41fd954182fd9cd58201f7b485d
Renzo Alva Principe <r.alvaprincipe@campus.unimib.it>
2016-01-17 23:00:01 +0100

    fix2 samples API

--- summarization/src/it/unimib/disco/summarization/web/Sample.java
+++ summarization/src/it/unimib/disco/summarization/web/Sample.java
@@ -13,9 +13,8 @@ public class Sample implements Api {
 		String[] tripla_info = getAKP(akpURI);
 		String queryString = buildQuery(tripla_info);
 		
-		Query query = QueryFactory.create(queryString);
-		QueryExecution qexec = QueryExecutionFactory.sparqlService(endpoint, query);
-		ResultSet results = qexec.execSelect();
+		SparqlEndpoint  externalEndpoint = SparqlEndpoint.external(endpoint);
+		ResultSet results = externalEndpoint.execute(queryString);
 		
 		ByteArrayOutputStream out = new ByteArrayOutputStream();	
 		ResultSetFormatter.outputAsJSON(out, results);
@@ -25,78 +24,44 @@ public class Sample implements Api {
 		return istream;
 	}
 	
-	public static String[] getAKP(String akpURI) {
+	private String[] getAKP(String akpURI) {
 		SparqlEndpoint localEndpoint = SparqlEndpoint.local();
 		ResultSet results;
-		int i, l, j, k;
-		String[] array = new String[3];
-		String[] tripla = new String[4];  //conterrà sogg, pred, ogg e property_type
+		String[] tripla_info = new String[4];  //conterrà sogg, pred, ogg e property_type
 		String property_type = "";
 		
+		String query = ("SELECT ?s ?p ?o ?predicate " +
+						"WHERE { " +
+						"		<" + akpURI + ">" + 
+						"						<http://www.w3.org/1999/02/22-rdf-syntax-ns#subject> ?subject;" + 
+						"						<http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate> ?predicate;" +
+						"						<http://www.w3.org/1999/02/22-rdf-syntax-ns#object> ?object." + 
+						"		?subject <http://www.w3.org/2000/01/rdf-schema#seeAlso> ?s." + 
+						"		?predicate <http://www.w3.org/2000/01/rdf-schema#seeAlso> ?p." +
+						"		?object <http://www.w3.org/2000/01/rdf-schema#seeAlso> ?o." +
+						"		}");
 		
-		results = localEndpoint.execute("DESCRIBE <" + akpURI + ">");
-		String result = ResultSetFormatter.asText(results); 
+		results = localEndpoint.execute(query);
+		QuerySolution solution = results.nextSolution();
+		tripla_info[0] = solution.get("s").toString();
+		tripla_info[1] = solution.get("p").toString();
+		tripla_info[2] = solution.get("o").toString();
+		tripla_info[3] = solution.get("predicate").toString();
 		
-		
-		i = result.indexOf("<http://www.w3.org/1999/02/22-rdf-syntax-ns#subject>");
-		l = "<http://www.w3.org/1999/02/22-rdf-syntax-ns#subject>".length();
-		String temp = result.substring(i+l);
-		j = temp.indexOf('<'); k = temp.indexOf('>');
-		array[0] = temp.substring(j, k+1);
-		
-		i = result.indexOf("<http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate>");
-		l = "<http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate>".length();
-		temp = result.substring(i+l);
-		j = temp.indexOf('<'); k = temp.indexOf('>');
-		array[1] = temp.substring(j, k+1);
-		
-		i = result.indexOf("<http://www.w3.org/1999/02/22-rdf-syntax-ns#object>");
-		l = "<http://www.w3.org/1999/02/22-rdf-syntax-ns#object>".length();
-		temp = result.substring(i+l);
-		j = temp.indexOf('<'); k = temp.indexOf('>');
-		array[2] = temp.substring(j, k+1);
-		
-		
-		if (array[2].contains("/www.w3.org/2000/01/rdf-schema#Literal"))
+		if (tripla_info[2].contains("/www.w3.org/2000/01/rdf-schema#Literal"))
 			property_type = "datatype-property_Literal";
-		else if ( array[1].contains("datatype-property"))
-			property_type = "datatype";
+		else if (tripla_info[3].contains("datatype-property"))
+			property_type = "datatype-property";
 		else
 			property_type = "object-property";
-		tripla[3] = property_type;
 		
+		tripla_info[3] = property_type;
 		
-		
-		results = localEndpoint.execute("SELECT ?o " +
-									 	 "WHERE { "   +
-									 	 array[0] + " <http://www.w3.org/2000/01/rdf-schema#seeAlso> ?o" +
-										 "}");
-		String res = ResultSetFormatter.asText(results);
-		j = res.indexOf('<'); k = res.indexOf('>');
-		tripla[0] = res.substring(j+1, k);
-		
-		results = localEndpoint.execute("SELECT ?o " +
-									 	 "WHERE { "   +
-									 	 array[1] + " <http://www.w3.org/2000/01/rdf-schema#seeAlso> ?o" +
-										 "}");
-		res = ResultSetFormatter.asText(results);
-		j = res.indexOf('<'); k = res.indexOf('>');
-		tripla[1] = res.substring(j+1, k);
-		
-		results = localEndpoint.execute("SELECT ?o " +
-			 	 						"WHERE { "   +
-			 	 						array[2] + " <http://www.w3.org/2000/01/rdf-schema#seeAlso> ?o" +
-				 						"}");
-		res = ResultSetFormatter.asText(results);
-		j = res.indexOf('<'); k = res.indexOf('>');
-		tripla[2] = res.substring(j+1, k);
-	
-		return tripla;
+		return tripla_info;
 	}
 	
-	
 		
-	public String buildQuery(String[] tripla_info) {
+	private String buildQuery(String[] tripla_info) {
 		String query;
 		if(tripla_info[3].equals("object-property")){
 			query = "SELECT DISTINCT ?s ?p ?o " +
@@ -124,6 +89,7 @@ public class Sample implements Api {
 					"       BIND( <"+ tripla_info[1] + "> AS ?p)." +
 					"      }LIMIT 30";
 		}
+		
 		return query;
 	}
 }
